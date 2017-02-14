744fcadb1de65e0465aad3c14213232930bb958f
demirramazan <rdemir4434@gmail.com>
2017-02-14 16:39:15 +0300

    view scope example

--- src/bean/ViewScopedBean.java
+++ src/bean/ViewScopedBean.java
@@ -1,7 +1,64 @@
 package bean;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+
 /**
  * Created by ramazan.demir on 14.2.2017.
  */
-public class ViewScopedBean {
+@ManagedBean
+@ViewScoped
+public class ViewScopedBean implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+
+    private String name;
+    private String surname;
+    private String pass;
+    private int counter;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public int getCounter() {
+        return ++counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public String getGreating(){
+        if (name!=null &&name.length()>0){
+            return "welcome ..." + name+" "+surname;
+        }else{
+            return "";
+        }
+    }
+    public int getHashCode(){
+        return super.hashCode();
+    }
+
 }
